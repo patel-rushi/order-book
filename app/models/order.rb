114cfca28975a572329eddf5e0e386eb3c212cac
Rushi Patel <mail.rushipatel@gmail.com>
2024-04-30 13:49:03 -0400

    search logic

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
-   
+    scope :top_bids_for, ->(symbol, time) {
+        where(symbol: symbol)
+        .where('"startTime" <= ? AND "endTime" >= ?', time, time)
+        .order(bidPrice: :desc)
+        .limit(5)
+    }
+
+    scope :top_asks_for, ->(symbol, time) {
+        where(symbol: symbol)
+        .where('"startTime" <= ? AND "endTime" >= ?', time, time)
+        .order(askPrice: :asc)
+        .limit(5)
+    }
 end
